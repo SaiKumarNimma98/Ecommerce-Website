b2262c04b99afc260ddc5431f0ebb0354afc3559
SaiKumarNimma98 <saikumarnimma98@gmail.com>
2024-11-09 21:48:43 +0530

    Add files via upload

---
+++ ecom-proj/src/main/java/com/telusko/ecom_proj/model/Product.java
@@ -0,0 +1,42 @@
+package com.telusko.ecom_proj.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageDate;
+
+//    public void setImageName(String originalFilename) {
+//
+//    }
+//
+//    public void setImageType(String contentType) {
+//    }
+//
+//    public void setImageDate(byte[] bytes) {
+//    }
+}
---
+++ ecom-proj/src/main/resources/data1.sql
@@ -0,0 +1,6 @@
+INSERT INTO product(name, desc, brand, price, category, release_date, available, quantity) VALUES
+('TATA Nexon', 'A compact SUV', 'TATA', 750000.00, 'Car', '2024-01-01', true, 50),
+('Maruthi Swift', 'A Hatchpack Car', 'Maruthi', 550000.00, 'Car', '2024-02-01', true, 100),
+('Hyundai Creta ', 'A compact SUV', 'Hyundai', 950000.00, 'Car', '2024-03-01', true, 75),
+('Mahindra Thar', 'Off Road Vehicle', 'Mahindra', 1200000.00, 'Car', '2024-04-01', true, 30),
+('Honda City', 'A Sedan Car', 'Honda', 1100000.00, 'Car', '2024-05-01', true, 60);
